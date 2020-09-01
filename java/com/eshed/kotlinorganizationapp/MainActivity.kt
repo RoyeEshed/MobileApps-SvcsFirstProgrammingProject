561ddd568b7e9e521a477977fb14c8cbfba944c7
RoyeEshed <70332452+RoyeEshed@users.noreply.github.com>
2020-09-01 16:04:33 -0400

    Add files via upload

---
+++ java/com/eshed/kotlinorganizationapp/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.eshed.kotlinorganizationapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+
+        loginButton.setOnClickListener {
+            val username = usernameEditText.text.toString()
+            val password = passwordEditText.text.toString()
+
+            Log.d("MainActivity","Password is " + password)
+            Log.d("MainActivity","Username is " + username)
+        }
+
+        registerAccountTextView.setOnClickListener {
+            Log.d("MainActivity","try to show log in activity")
+
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
